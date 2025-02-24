a93321ee13eac8a821733ab9a97bbb6a62c0ad07
Ameb8 <103926493+Ameb8@users.noreply.github.com>
2025-02-24 10:35:41 -0800

    Add files via upload
    Updated density approximation calculation using distance matrix

--- CUDA/DensityApproximation.cu
+++ CUDA/DensityApproximation.cu
@@ -6,14 +6,14 @@
 
 //device function to partition array
 __device__ int partition(double* dist, int left, int right) {
-    double pivot = dist[right];  //use rightmost element as pivot
+    double pivot = dist[right];  //ue rightmost element as pivot
     int i = left - 1;  //pointer to the smaller element
 
     //swap elements around pivot
     for (int j = left; j <= right - 1; j++) {
         if (dist[j] <= pivot) {
             i++;
-            // Swap dist[i] and dist[j]
+            //swap dist[i] and dist[j]
             double temp = dist[i];
             dist[i] = dist[j];
             dist[j] = temp;
@@ -25,7 +25,7 @@ __device__ int partition(double* dist, int left, int right) {
     dist[i + 1] = dist[right];
     dist[right] = temp;
 
-    return (i + 1);  // Return partition index
+    return (i + 1);  //return partition index
 }
 
 //device function to get distance to k-th nearest element
@@ -45,21 +45,30 @@ __device__ double quickselect(double* dist, int left, int right, int k) {
     }
 }
 
-//computes density of single point for each trhead
-__global__ void kNNDensity(const double* data, double* result, int N, int k) {
+//computes density of single point for each thread
+__global__ void kNNDensity(double* distMatrix, double* result, int N, int k) {
     int i = blockIdx.x * blockDim.x + threadIdx.x; //get index
     
     if (i < N) {
-        //get distances
-        double dist[N]; //allocate local memory to store distances
-        for(int j = 0; j < N; j++)
-            dist[j] = fabs(data[i] - data[j]);
-
+        //get distances from matrix
+        double* row = &distMatrix[i * N];
+        
         //get distance to k-th nearest neighbor
-        double Ri = quickselect(dist, 0, N - 1, k);
+        double Ri = quickselect(row, 0, N - 1, k);
 
         //compute density approximation
         result[i] = static_cast<double>(k) / (N * 2 * Ri);
+    }
+}
+
+//computes distance between every pair of elements in individual threads
+__global__ void computeDistances(const double* data, double* distanceMatrix, int N) {
+    int i = blockIdx.y * blockDim.y + threadIdx.y;  //row index
+    int j = blockIdx.x * blockDim.x + threadIdx.x;  //column index
+
+    if(i < N && j < N) {
+        //calculate distance between pair
+        distanceMatrix[i * N + j] = fabs(data[i] - data[j]);
     }
 }
 
@@ -72,28 +81,39 @@ std::vector<double> getDensity(const std::vector<double>& data, int k) {
         k = N - 1;
 
     //allocate device memory
-    double *d_data, /* *d_dist, */ *d_result;
+    double *d_data, *d_distMatrix, *d_result;
     cudaMalloc(&d_data, N * sizeof(double)); //store input data
     cudaMalloc(&d_result, N * sizeof(double)); //store the result for each data point
+    cudaMalloc(&d_distMatrix, N * N * sizeof(double)); //store distances between all points in vector
     
     //copy input data to device
     cudaMemcpy(d_data, data.data(), N * sizeof(double), cudaMemcpyHostToDevice);
 
-    //launch CUDA kernel
+    //set blocks and block size for distance matrix calculation
+    dim3 threadsPerBlock(16, 16);  // 16x16 threads per block
+    dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                       (N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    //launch kernel to calculate distances
+    computeDistances<<<blocksPerGrid, threadsPerBlock>>>(d_data, d_distMatrix, N);
+    cudaDeviceSynchronize();
+    
+    //set block/thread size for density approximation
     int blockSize = 256;
     int numBlocks = (N + blockSize - 1) / blockSize;
     
     //calculate each elements density approximation in individual threads
-    kNNDensity<<<numBlocks, blockSize>>>(d_data, d_result, N, k);
+    kNNDensity<<<numBlocks, blockSize>>>(d_distMatrix, d_result, N, k);
     cudaDeviceSynchronize(); //wait for all threads to finish execution
 
     //copy result back to host
     std::vector<double> host_result(N);
     cudaMemcpy(host_result.data(), d_result, N * sizeof(double), cudaMemcpyDeviceToHost);
-
+    
     //free device memory
     cudaFree(d_data);
     cudaFree(d_result);
+    cudaFree(d_distMatrix);
 
     return host_result;
 }
@@ -110,6 +130,7 @@ int main() {
     std::vector<double> data = {3.0, 3.0, 20.0, 17.0, 7.0};
     printf("input:\n");
     printVector(data);
+
 
     std::vector<double> density = getDensity(data, 2);
     printf("density result:\n");
